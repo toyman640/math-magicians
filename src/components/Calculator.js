07ea5186d88841629cf9f2e0581fafa160fcbc9d
toyman640 <falakoadebayor@gmail.com>
2023-06-07 16:07:21 +0100

    Add logic to calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,46 +1,63 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
+
 function Calculator() {
+  const [calculatorState, setCalculatorState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const handleLogic = (buttonValue) => {
+    const newState = calculate(calculatorState, buttonValue);
+    setCalculatorState(newState);
+  };
   return (
     <div className="Container">
-      <Screen />
+      <Screen result={calculatorState.next || calculatorState.total || '0'} />
       <div className="Row">
-        <button className="Other" type="button">AC</button>
-        <button className="Other" type="button">+/-</button>
-        <button className="Other" type="button">%</button>
-        <button type="button" className="Operator">&#247;</button>
+        <button onClick={() => handleLogic('AC')} className="Other" type="button">AC</button>
+        <button onClick={() => handleLogic('+/-')} className="Other" type="button">+/-</button>
+        <button onClick={() => handleLogic('%')} className="Other" type="button">%</button>
+        <button onClick={() => handleLogic('÷')} type="button" className="Operator">÷</button>
       </div>
       <div className="Row">
-        <button className="Other" type="button">7</button>
-        <button className="Other" type="button">8</button>
-        <button className="Other" type="button">9</button>
-        <button type="button" className="Operator">x</button>
+        <button onClick={() => handleLogic('7')} className="Other" type="button">7</button>
+        <button onClick={() => handleLogic('8')} className="Other" type="button">8</button>
+        <button onClick={() => handleLogic('9')} className="Other" type="button">9</button>
+        <button onClick={() => handleLogic('x')} type="button" className="Operator">x</button>
       </div>
       <div className="Row">
-        <button className="Other" type="button">4</button>
-        <button className="Other" type="button">5</button>
-        <button className="Other" type="button">6</button>
-        <button type="button" className="Operator">-</button>
+        <button onClick={() => handleLogic('4')} className="Other" type="button">4</button>
+        <button onClick={() => handleLogic('5')} className="Other" type="button">5</button>
+        <button onClick={() => handleLogic('6')} className="Other" type="button">6</button>
+        <button onClick={() => handleLogic('-')} type="button" className="Operator">-</button>
       </div>
       <div className="Row">
-        <button className="Other" type="button">1</button>
-        <button className="Other" type="button">2</button>
-        <button className="Other" type="button">3</button>
-        <button type="button" className="Operator">+</button>
+        <button onClick={() => handleLogic('1')} className="Other" type="button">1</button>
+        <button onClick={() => handleLogic('2')} className="Other" type="button">2</button>
+        <button onClick={() => handleLogic('3')} className="Other" type="button">3</button>
+        <button onClick={() => handleLogic('+')} type="button" className="Operator">+</button>
       </div>
       <div className="Row">
-        <button className="Zero" type="button">0</button>
-        <button className="Other" type="button">.</button>
-        <button className="Operator" type="button">=</button>
+        <button onClick={() => handleLogic('0')} className="Zero" type="button">0</button>
+        <button onClick={() => handleLogic('.')} className="Other" type="button">.</button>
+        <button onClick={() => handleLogic('=')} className="Operator" type="button">=</button>
       </div>
     </div>
   );
 }
 
-function Screen() {
+function Screen({ result }) {
   return (
     <div className="Screen">
-      <span className="Digit">0</span>
+      <span className="Digit">{result}</span>
     </div>
   );
 }
+
+Screen.propTypes = {
+  result: PropTypes.string.isRequired,
+};
 
 export default Calculator;
