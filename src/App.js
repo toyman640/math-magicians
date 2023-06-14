edaa06af1bc3229a1a7cc76e4fdd4550d2ce2caa
toyman640 <falakoadebayor@gmail.com>
2023-06-14 10:52:23 +0100

    Add route and 2 pages to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import {
+  Routes, Route, Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quotes />
-    </div>
+    <>
+      <nav className="Navigation">
+        <h1 className="Logo">Math Magicians</h1>
+        <ul>
+          <li>
+            <Link className="Link" to="/">Home</Link>
+          </li>
+          |
+          <li>
+            <Link className="Link" to="/calculator">Calculator</Link>
+          </li>
+          |
+          <li>
+            <Link className="Link" to="/quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quotes />} />
+      </Routes>
+    </>
   );
 }
 
